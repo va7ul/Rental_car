f733f6c6621472cc74f91a05402a9e61a979a12a
va7ul <vas7ul@gmail.com>
2023-12-07 22:07:17 +0200

    add axios for api

---
+++ src/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://6519986e818c4e98ac608cb9.mockapi.io';
+
+export const getAdvert = async () => {
+  const response = await axios.get('/adverts');
+  return response.data;
+};
