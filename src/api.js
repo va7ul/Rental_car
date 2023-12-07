7b8376801c9488ceda9491ae87a29772ee088b59
va7ul <vas7ul@gmail.com>
2023-12-07 23:33:51 +0200

    create adverts slice

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://6519986e818c4e98ac608cb9.mockapi.io';
 
-export const getAdvert = async () => {
+export const getAdverts = async () => {
   const response = await axios.get('/adverts');
   return response.data;
 };
