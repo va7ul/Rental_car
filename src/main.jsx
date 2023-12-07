1cfcb5d889eb6819a94749c52aaf398a989f37cc
va7ul <vas7ul@gmail.com>
2023-12-07 23:31:43 +0200

    create redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { GlobalStyle } from './GlobalStyle.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+import { store, persistor } from './redux/store.js';
+import { GlobalStyle } from './GlobalStyle.js';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Rental_car">
-      <App />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/Rental_car">
+          <App />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
