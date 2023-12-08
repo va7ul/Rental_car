ed9a1ff4113cc405e26054bc4ba0ac63841f8097
va7ul <vas7ul@gmail.com>
2023-12-09 00:47:00 +0200

    update catalog page

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,5 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAdverts } from '../redux/operations';
+import { selectIsLoading, selectError } from '../redux/selectors';
+import { Filter } from '../components/Filter/Filter';
+import { AdvertsList } from '../components/AdvertsList/AdvertsList';
+
 const CatalogPage = () => {
-  return <div>CatalogPage</div>;
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchAdverts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>CatalogPage</h2>
+      <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <AdvertsList />
+    </div>
+  );
 };
 
 export default CatalogPage;
---
+++ src/redux/selectors.js
@@ -0,0 +1,7 @@
+export const selectIsLoading = state => state.adverts.isLoading;
+
+export const selectError = state => state.adverts.error;
+
+export const selectAdverts = state => state.adverts.items;
+
+// export const selectError = state => state.adverts.error;
