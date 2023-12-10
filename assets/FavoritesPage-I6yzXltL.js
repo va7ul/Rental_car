fadf04a31a9483145abb21c674946b5076576768
va7ul <va7ul@users.noreply.github.com>
2023-12-10 17:17:13 +0000

    deploy: 7f348a8bb314d6eeb56727919346ba87af73fd81

--- assets/FavoritesPage-I6yzXltL.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-7RTp454p.js";const t=()=>e.jsx("div",{children:"FavoritesPage"});export{t as default};
---
+++ assets/HomePage-8iyoHRu9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-31xUZK2a.js";const r=()=>e.jsx("div",{children:"HomePage"});export{r as default};
