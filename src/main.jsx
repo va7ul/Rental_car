90ae568aa1850fdc1ed3e46852611a0813dfe599
va7ul <vas7ul@gmail.com>
2023-12-11 19:38:43 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store.js';
 import { GlobalStyle } from './GlobalStyle.js';
 import App from './App.jsx';
+
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/Rental_car/">
+        <RouterProvider router={router}>
           <App />
-          <GlobalStyle />
-        </BrowserRouter>
+        </RouterProvider>
+        <GlobalStyle />
       </PersistGate>
     </Provider>
   </React.StrictMode>
