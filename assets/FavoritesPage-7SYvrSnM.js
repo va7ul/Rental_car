6f5312a5b97fa7b25bdbed3586b2ad8cf79f22dc
va7ul <va7ul@users.noreply.github.com>
2023-12-11 15:05:37 +0000

    deploy: d864128d5f6bb16df7f1f88abff650056eeae7cc

---
+++ assets/FavoritesPage-7SYvrSnM.js
@@ -0,0 +1,5 @@
+import{s as c,b as a,j as s,c as o,r as d,f as l}from"./index-EYnY2bny.js";import{s as p,e as n,A as x}from"./AdvertsCard-hUUj1okR.js";const f=c.ul`
+  display: flex;
+  gap: 50px 29px;
+  flex-wrap: wrap;
+`,v=()=>{const t=a(p),i=a(n);let r=[];return r=t.filter(e=>i.includes(e.id)),s.jsx(f,{children:r.map(e=>s.jsx("li",{children:s.jsx(x,{item:e})},e.id))})},h=()=>{const t=o();return d.useEffect(()=>{t(l())},[t]),s.jsx("div",{children:s.jsx(v,{})})};export{h as default};
--- assets/HomePage-pxG3QKov.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CyPfqvJF.js";const r=()=>e.jsx("div",{children:"HomePage"});export{r as default};
