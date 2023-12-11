7b4b2570387c8fbe9c8f35e2cfb2689dd53bc5cb
va7ul <vas7ul@gmail.com>
2023-12-11 18:07:26 +0200

    finish home page

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/Rental_car">
+        <BrowserRouter basename="/Rental_car/">
           <App />
           <GlobalStyle />
         </BrowserRouter>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
+import Cars from '../images/Cars.png';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <img src={Cars} style={{ width: '100%' }} />
+      <h1 style={{ fontSize: '60px' }}>Rent Your Car Now</h1>
+    </div>
+  );
 };
 
 export default HomePage;
