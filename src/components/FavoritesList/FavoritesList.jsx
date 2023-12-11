d78bec3d63a9c998ed4973368010cc22d72a20f6
va7ul <vas7ul@gmail.com>
2023-12-11 19:32:30 +0200

    finish favorites

--- src/components/FavoritesList/FavoritesList.jsx
+++ src/components/FavoritesList/FavoritesList.jsx
@@ -1,15 +1,7 @@
-import { useSelector } from 'react-redux';
 import { AdvertsCard } from '../AdvertsCard/AdvertsCard';
-import { selectFavorites, selectAdverts } from '../../redux/selectors';
 import { List } from './FavoritesList.styled';
 
-export const FavoritesList = () => {
-  const adverts = useSelector(selectAdverts);
-  const favoritesId = useSelector(selectFavorites);
-
-  let favoritesAdverts = [];
-  favoritesAdverts = adverts.filter(item => favoritesId.includes(item.id));
-
+export const FavoritesList = ({ favoritesAdverts }) => {
   return (
     <List>
       {favoritesAdverts.map(item => (
--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,16 +1,46 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { FavoritesList } from '../components/FavoritesList/FavoritesList';
+import { selectFavorites, selectAdverts } from '../redux/selectors';
 import { fetchAdverts } from '../redux/operations';
+import noResults from '../images/no-results.png';
 
 const FavoritesPage = () => {
   const dispatch = useDispatch();
+  const adverts = useSelector(selectAdverts);
+  const favoritesId = useSelector(selectFavorites);
+
+  let favoritesAdverts = [];
+  favoritesAdverts = adverts.filter(item => favoritesId.includes(item.id));
 
   useEffect(() => {
     dispatch(fetchAdverts());
   }, [dispatch]);
 
-  return <div>{<FavoritesList />}</div>;
+  return (
+    <>
+      {favoritesAdverts.length > 0 ? (
+        <FavoritesList favoritesAdverts={favoritesAdverts} />
+      ) : (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <img
+            src={noResults}
+            style={{
+              width: '50%',
+              height: '50%',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default FavoritesPage;
