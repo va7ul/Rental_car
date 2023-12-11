3f63e587f8a5a725120fc09faee8ca0f083ad387
va7ul <va7ul@users.noreply.github.com>
2023-12-11 17:49:12 +0000

    deploy: 966a96857637456532f826a2317b300ca14e9919

--- assets/FavoritesPage-UairwQbk.js
+++ assets/FavoritesPage-UairwQbk.js
@@ -1,4 +1,4 @@
-import{s as l,j as s,c,b as a,r as o,f as d}from"./index-lb1IasGz.js";import{A as x,s as p,e as f}from"./AdvertsCard-DZu9EfhX.js";const u=l.ul`
+import{s as l,j as s,c,b as a,r as o,f as d}from"./index-g64uWIcD.js";import{A as x,s as p,e as f}from"./AdvertsCard-9_Gjd45g.js";const u=l.ul`
   display: flex;
   gap: 50px 29px;
   flex-wrap: wrap;
