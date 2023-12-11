83f990a321e4275291c2bfdce9ccc8d5ff9b35b7
va7ul <va7ul@users.noreply.github.com>
2023-12-11 17:33:03 +0000

    deploy: d78bec3d63a9c998ed4973368010cc22d72a20f6

--- assets/FavoritesPage-Wuk2d8S9.js
+++
@@ -1,5 +0,0 @@
-import{s as c,b as a,j as s,c as o,r as d,f as l}from"./index-a7iivIEP.js";import{s as p,e as n,A as x}from"./AdvertsCard-9jyEvmBq.js";const f=c.ul`
-  display: flex;
-  gap: 50px 29px;
-  flex-wrap: wrap;
-`,v=()=>{const t=a(p),i=a(n);let r=[];return r=t.filter(e=>i.includes(e.id)),s.jsx(f,{children:r.map(e=>s.jsx("li",{children:s.jsx(x,{item:e})},e.id))})},h=()=>{const t=o();return d.useEffect(()=>{t(l())},[t]),s.jsx("div",{children:s.jsx(v,{})})};export{h as default};
---
+++ assets/FavoritesPage-eFEIDVQI.js
@@ -0,0 +1,5 @@
+import{s as l,j as s,c,b as a,r as o,f as d}from"./index-bNuiEDF-.js";import{A as x,s as p,e as f}from"./AdvertsCard-k_To4inJ.js";const u=l.ul`
+  display: flex;
+  gap: 50px 29px;
+  flex-wrap: wrap;
+`,h=({favoritesAdverts:t})=>s.jsx(u,{children:t.map(e=>s.jsx("li",{children:s.jsx(x,{item:e})},e.id))}),j="/Rental_car/assets/no-results-nrrqrLxK.png",m=()=>{const t=c(),e=a(p),i=a(f);let r=[];return r=e.filter(n=>i.includes(n.id)),o.useEffect(()=>{t(d())},[t]),s.jsx(s.Fragment,{children:r.length>0?s.jsx(h,{favoritesAdverts:r}):s.jsx("div",{style:{display:"flex",justifyContent:"center"},children:s.jsx("img",{src:j,style:{width:"50%",height:"50%",display:"flex",justifyContent:"center"}})})})};export{m as default};
