753e425daf1b86b0750cb1b56c6533d6b72d382b
va7ul <vas7ul@gmail.com>
2023-12-11 19:55:38 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://6519986e818c4e98ac608cb9.mockapi.io';
-const page = 1;
-const limit = 12;
 
 export const getAdverts = async () => {
-  const response = await axios.get(`/adverts?page=${page}&limit=${limit}`);
+  const response = await axios.get('/adverts');
   return response.data;
 };
